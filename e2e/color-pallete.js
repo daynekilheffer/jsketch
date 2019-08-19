b2714d5a66811c142f19c80549877c525d2e6b3e
dayne <dayne@scoir.com>
2019-08-19 07:54:24 -0400

    add color pallete to sketch file

---
+++ e2e/color-pallete.js
@@ -0,0 +1,16 @@
+import fs from 'fs'
+import { File, RawJSONLayer, Artboard } from '../lib'
+
+const file = new File()
+const page = file.document.newPage()
+page.name = 'royal purple'
+
+file.document.addColor({red: 92, green: 37, blue: 127}, 'dark purple')
+file.document.addColor({red: 214, green: 151, blue: 255}, 'light purple')
+file.document.addColor({red: 184, green: 75, blue: 255}, 'pure purple')
+
+file.toStream()
+  .pipe(fs.createWriteStream('color-pallete.sketch'))
+  .on('finish', function () {
+      console.log("sketch file written");
+  });
